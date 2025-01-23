bd3c77735e5b6bcbb8a68b7f2416b52f431ff6a6
עמית אורן <oren7@auth.ad.bgu.ac.il>
2025-01-23 15:00:32 +0200

    Amit commit 23/1

--- SystemAnalysisAndDesignProject/DriverIdCopyForm.cs
+++ SystemAnalysisAndDesignProject/DriverIdCopyForm.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,17 +23,26 @@ namespace SystemAnalysisAndDesignProject
             this.driverAccountForm = driverAccountForm;
         }
 
-        private void DriverIdCopyForm_Load(object sender, EventArgs e)
-        {
-            string personName = driver.GetName();
-            label1.Text = $"{personName} ID Copy:";
-            //pictureBox1.Image = Image.FromFile("C:\\Users\\oren7\\Downloads\\SystemAnalysisAndDesignProject\\IdCopy.jpg");
-        }
-
         private void back_Click(object sender, EventArgs e)
         {
             this.Close();
             this.driverAccountForm.Show();
+        }
+
+
+        private void DriverIdCopyForm_Load(object sender, EventArgs e)
+        {
+            string personName = driver.GetName();
+            label1.Text = $"{personName} ID Copy:";
+            string imageUrl = this.driver.GetIdCopy();
+                       using (WebClient client = new WebClient())
+            {
+                byte[] imageBytes = client.DownloadData(imageUrl);
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    pictureBox1.Image = Image.FromStream(ms);
+                }
+            }
         }
     }
 }
---
+++ SystemAnalysisAndDesignProject/DriverWorkCertification.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SystemAnalysisAndDesignProject
+{
+    partial class DriverWorkCertification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DriverWorkCertification));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelOrderId = new System.Windows.Forms.Label();
+            this.labelFinishDate = new System.Windows.Forms.Label();
+            this.labelStartDate = new System.Windows.Forms.Label();
+            this.labelCustomer = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(507, 483);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelOrderId
+            // 
+            this.labelOrderId.AutoSize = true;
+            this.labelOrderId.Font = new System.Drawing.Font("Segoe UI Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOrderId.Location = new System.Drawing.Point(64, 111);
+            this.labelOrderId.Name = "labelOrderId";
+            this.labelOrderId.Size = new System.Drawing.Size(76, 25);
+            this.labelOrderId.TabIndex = 8;
+            this.labelOrderId.Text = "OrderID";
+            // 
+            // labelFinishDate
+            // 
+            this.labelFinishDate.AutoSize = true;
+            this.labelFinishDate.Font = new System.Drawing.Font("Segoe UI Light", 9F);
+            this.labelFinishDate.Location = new System.Drawing.Point(248, 273);
+            this.labelFinishDate.Name = "labelFinishDate";
+            this.labelFinishDate.Size = new System.Drawing.Size(61, 15);
+            this.labelFinishDate.TabIndex = 11;
+            this.labelFinishDate.Text = "Finish Date";
+            // 
+            // labelStartDate
+            // 
+            this.labelStartDate.AutoSize = true;
+            this.labelStartDate.Font = new System.Drawing.Font("Segoe UI Light", 9F);
+            this.labelStartDate.Location = new System.Drawing.Point(67, 273);
+            this.labelStartDate.Name = "labelStartDate";
+            this.labelStartDate.Size = new System.Drawing.Size(59, 15);
+            this.labelStartDate.TabIndex = 10;
+            this.labelStartDate.Text = "Start Date";
+            // 
+            // labelCustomer
+            // 
+            this.labelCustomer.AutoSize = true;
+            this.labelCustomer.Font = new System.Drawing.Font("Segoe UI Light", 9F);
+            this.labelCustomer.Location = new System.Drawing.Point(67, 182);
+            this.labelCustomer.Name = "labelCustomer";
+            this.labelCustomer.Size = new System.Drawing.Size(56, 15);
+            this.labelCustomer.TabIndex = 9;
+            this.labelCustomer.Text = "Customer";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 177);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(70, 316);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(70, 339);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(70, 362);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(15, 14);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 9F);
+            this.label1.Location = new System.Drawing.Point(66, 442);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Driver";
+            // 
+            // DriverWorkCertification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(517, 507);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelFinishDate);
+            this.Controls.Add(this.labelStartDate);
+            this.Controls.Add(this.labelCustomer);
+            this.Controls.Add(this.labelOrderId);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "DriverWorkCertification";
+            this.Text = "DriverWorkCertification";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelOrderId;
+        private System.Windows.Forms.Label labelFinishDate;
+        private System.Windows.Forms.Label labelStartDate;
+        private System.Windows.Forms.Label labelCustomer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}

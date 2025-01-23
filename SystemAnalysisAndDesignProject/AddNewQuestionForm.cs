edfca796332c36ddc55c3fa36644b282ffd09cda
RomiMali22 <romimali84@gmail.com>
2025-01-23 23:52:47 +0200

    Update AddNewQuestionForm.cs
    Signed-off-by: RomiMali22 <romimali84@gmail.com>

--- SystemAnalysisAndDesignProject/AddNewQuestionForm.cs
+++ SystemAnalysisAndDesignProject/AddNewQuestionForm.cs
@@ -14,14 +14,14 @@ namespace SystemAnalysisAndDesignProject
     {
         private OfficeManager officeManager;
         private EditSurvey originEditSurveyForm;
-
+            
         public AddNewQuestionForm(OfficeManager officeManager, EditSurvey originEditSurveyForm)
         {
             this.officeManager = officeManager;
             this.originEditSurveyForm = originEditSurveyForm;
             InitializeComponent();
         }
-
+         //when the office manager wants to add a new question to the survey
         private void AddNewQuestionForm_Load(object sender, EventArgs e)
         {
             NewQuestionCard card = new NewQuestionCard();

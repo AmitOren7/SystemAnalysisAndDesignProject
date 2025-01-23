b23c5220b94eb6751febeb0d6e1b8783a3b9d816
eden2465 <142608067+eden2465@users.noreply.github.com>
2025-01-24 00:06:30 +0200

    Update Evaluatable.cs
    Signed-off-by: eden2465 <142608067+eden2465@users.noreply.github.com>

--- SystemAnalysisAndDesignProject/Evaluatable.cs
+++ SystemAnalysisAndDesignProject/Evaluatable.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace SystemAnalysisAndDesignProject
 {
     public interface Evaluatable
-    {
+    { //this interface references drivers and clerks only since they are the employees who are evaluated through the surveys.
 
         string GetName();
         string GetId();
